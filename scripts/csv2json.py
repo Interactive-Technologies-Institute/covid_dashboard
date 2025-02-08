e18c547e3bd5a0f3a31e682cbf8f121c6ea73a2d
girlfromrio <acarolbaptista1@gmail.com>
2025-02-08 12:42:28 +0000

    cleaning

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -1,40 +1,6 @@
 import csv
 import json
 from datetime import datetime
-
-def processar_pois():
-    with open('./static/data/dados_localidades.csv', 'r', encoding="utf-8") as csvfile, \
-        open('./static/data/portugal_locations_output.json', 'w', encoding="utf-8") as jsonfile:
-        
-        output_json = {"type": "FeatureCollection", "features": []}
-
-        reader = csv.DictReader(csvfile, delimiter=';')  # Use o delimitador correto (;)
-
-        for row in reader:
-            try:
-                lon = float(row["lon"].replace(',', '.'))
-                lat = float(row["lat"].replace(',', '.'))
-            except ValueError:
-                #Ignorando linha com coordenadas inválidas
-                continue
-            
-            equip_value = row.get('Equip')
-            name_value = row.get('Name')  
-        
-            if equip_value and "Localidade" in equip_value and name_value.strip() and name_value != "NA":
-                inner_json = {
-                    "type": "Feature", 
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [lon, lat] 
-                    },
-                    "properties": {
-                        "title": name_value
-                    }
-                }
-                output_json["features"].append(inner_json)
-
-        json.dump(output_json, jsonfile, ensure_ascii=False, indent=4)
         
 
 def processar_casos():
@@ -68,7 +34,5 @@ def processar_casos():
 
         json.dump(output_json, jsonfile, ensure_ascii=False, indent=4)
 
-
-processar_pois()
-#processar_casos()
+processar_casos()
 

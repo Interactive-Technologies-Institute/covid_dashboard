555df523cdbe84d35c7091ec4bda7eb4dc394776
girlfromrio <acarolbaptista1@gmail.com>
2025-05-21 12:59:29 +0100

    fix bug

--- src/lib/components/Map/mapboxgl.ts
+++ src/lib/components/Map/mapboxgl.ts
@@ -1,6 +1,6 @@
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic29ja2l0b3MiLCJhIjoiY2t6NzUydWZwMGlyajJvbXVuZ2gyNTRjaiJ9.-fuONam95R59PjyGsEVdCQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5hcGlyZXMxMyIsImEiOiJjbWF4dmY2YjQwMnU4Mm1zaHlwd2J1YzNhIn0.-wCBtnB_dVsa6dnzAxi02w';
 
 const key = Symbol();
 
